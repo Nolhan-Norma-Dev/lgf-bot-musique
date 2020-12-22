061a3706fa9629f69ac78779d907d9716543f0f2
lagriculteur-normand <63206684+lagriculteur-normand@users.noreply.github.com>
2020-12-22 14:35:12 +0100

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DISCORD_TOKEN: 'NzQxMzQxMDY3ODc3ODEwMjU3.Xy2JrA.jI9qI2_vjqe8m58lWfAkgV2gKe8',
-    YOUTUBE_TOKEN: 'AIzaSyDskaJcAGvGjKrk2f9Q4bpLMQzjXV3zykw',
+    DISCORD_TOKEN: 'process.env.TOKEN',
+    YOUTUBE_TOKEN: 'process.env.YOUTUBE',
   };
